3467b62732d5df9e6ca92dc96753285053ab4cb4
liarmand <35567564+liarmand@users.noreply.github.com>
2018-10-04 12:10:26 +0300

    Update ex3.c

--- week7/ex3.c
+++ week7/ex3.c
@@ -3,40 +3,41 @@
 
 
 int main(){
-	//Accepting the size of array from user
-	printf("Enter size of array: ");
-	int nold;
-	scanf("%d", &nold);
+	// Allows user to specify the original array size, stored in variable n1.
+	printf("Enter original array size:");
+	int n1=0;
+	scanf("%d",&n1);
 
-	//Filling the array with 0`s and printing to be sure all is gone right
-	int *el1 = malloc(nold*sizeof(int));
-  	int i = 0;
-	for(i ; i < nold ; i++){
-		el1[i]=100;
+	//Create a new array of n1 ints
+	int* a1 = malloc(n1*sizeof(int));
+	int i;
+	for(i=0; i<n1; i++){
+		a1[i]=100;
 
-		printf("%d", el1[i]);
+		//Print each element out (to make sure things look right)
+		printf("%d", a1[i]);
 	}
 
-	//Accepting the new size of the array from user
-	printf("\nEnter new size of array : ");
-	int nnew;
-	scanf("%d",&nnew);
+	//User specifies the new array size, stored in variable n2.
+	printf("\nEnter new array size: ");
+	int n2=0;
+	scanf("%d",&n2);
 
-	//Dynamical change of array to new size
-	el1 = realloc(el1, nnew*sizeof(int));
+	//Dynamically change the array to size n2
+	a1 = realloc(el1, n2*sizeof(int));
 
-	// If size of new array bigger than older one
-  	for(int i = nold ; i < nnew ; i++){
-		el1[i]=0;
+	//If the new array is a larger size, set all new members to 0. Reason: dont want to use uninitialized variables.
+  	for(int i = n1 ; i < n2 ; i++){
+		a1[i]=0;
 	}
 
-	//Printing to be sure all is gone right
-	for(int i = 0 ; i < nnew ; i++){
-		printf("%d", el1[i]);
+	//Print each element out (to make sure things look right)
+	for(int i = 0 ; i < n2 ; i++){
+		printf("%d", a1[i]);
 	}
-	
 	printf("\n");
 
-	//end of the program
+	//Done with array now, done with program :D
+	
 	return 0;
 }
